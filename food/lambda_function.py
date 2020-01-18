2e298395b222396920cab340e78b870dfba62bba
Adrian Bao <adrianbao14@gmail.com>
2020-01-18 14:27:50 -0700

    Accept event data triggered by Alexa

---
+++ food/lambda_function.py
@@ -0,0 +1,54 @@
+"""
+Yelp API Lambda Function
+"""
+
+import json
+import requests
+import logging 
+import base64
+
+logger = logging.getLogger()
+
+CLIENT_ID = "zncWZ_dgf8To2adNhAF4nw"
+API_KEY = "4B0KBKAjf7_iXHZOV4XToA4tRBH72q-IinOncFSvEEc0_phmYVssCUYodQPoJXwkCo6TwFx4G4z-o4oTDII3wgsYFU_aikS5BlTbqRHICn4pAG5JF0HSs8vVplwjXnYx"
+API_ENDPOINT = "https://api.yelp.com/v3/businesses/search"
+
+cost_values = {
+    "$": 7,
+    "$$": 10,
+    "$$$": 20,
+    "$$$$": 25,
+    "$$$$$": 30 
+}
+
+def lambda_handler(event, context):
+    print(event)
+    logger.info("Event: {}".format(event))
+
+    base64_bytes = event["body"].encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    message = message_bytes.decode('ascii')
+    logger.info("Body: {}".format(message))
+    print("Message: {}".format(message))
+
+    # Sample Params
+    params = {
+        "location": "Tucson"
+    }
+
+    requestHeaders = {
+        "Content-type": "application/json",
+        "Authorization": "Bearer " + API_KEY # Required for Auth
+    }
+
+    response = requests.get(API_ENDPOINT, 
+        params=params, 
+        headers=requestHeaders
+        )
+
+    # Example of data access
+    response_json = json.loads(response.text)
+    dollar_signs = response_json["businesses"][0]["price"]
+
+    print("Dollar Signs: {}".format(dollar_signs))
+    print("Average Cost: {}".format(cost_values[dollar_signs]))
