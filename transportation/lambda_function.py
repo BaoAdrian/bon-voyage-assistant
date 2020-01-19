3f20c15ca7354abac9e038820a014b30fd2b0720
Trey Bryant <harrylbryant@email.arizona.edu>
2020-01-19 01:31:39 -0700

    transportation stuff

--- transportation/lambda_function.py
+++ transportation/lambda_function.py
@@ -1,33 +1,65 @@
 """
-XXXXX API Lambda Function
+Transportation API Lambda Function
 """
 
 import json
-import requests
 
 CLIENT_ID = ""
 API_KEY = ""
 API_ENDPOINT = ""
 
-def main():
-    # Sample Params
-    params = {
-        "location": "Tucson"
+def lambda_handler(json_data, context):
+    city, state = "Tucson", "Arizona"
+    city_trans = json_data["Tucson"]
+    trans_dict = extract_options(city_trans)
+    most_available = most_available_transport(trans_dict)
+    text = "One of the most available modes of transport in {}, {} is by {}, with an average cost of {} dollars!".format(city, state, most_available[0], most_available[1])
+
+    print(text)
+
+    returned_body = {
+        "transportation": city_trans,
+        "most_available": most_available[1],
+        "text": text
     }
 
-    requestHeaders = {
-        "Content-type": "application/json",
-        "Authorization": "Bearer " + API_KEY # Required for Auth
+    return {
+        "statusCode": 200,
+        "body": json.dumps(returned_body)
     }
 
-    response = requests.get(API_ENDPOINT, 
-        params=params, 
-        headers=requestHeaders
-    )
+# returns the most available transportation; the name of the method of transport, followed by average cost
+def most_available_transport(trans_dict):
+    most = trans_dict[0]["availability"]
+    method = trans_dict[0]["name"]
+    for t in trans_dict:
+        if (t["availability"] > most):
+            most = t["cost"]
+            method = t["name"]
 
-    # Example of data access
-    response_json = json.loads(response.text)
+    return (method, most)
 
+def extract_options(city):
+    """
+    Extracts all the returned places and formats a 
+    list of dictionaries containing only the data the 
+    is needed.
+
+    @param response_json Full JSON object returned from GET Request
+    @return formatted list of parsed JSON data
+    """
+    transportation = []
+    for t in city:
+        transportation.append(
+            {
+                "name": t["name"],
+                "cost": t["cost"],
+                "availability": t["availability"]
+            }
+        )
+    return transportation
 
 if __name__ == "__main__":
-    main()
+    with open("transportation/transport.json", "r") as f:
+        data = json.load(f)
+        lambda_handler(data, None)
